fed795aaa6b678bdd5532b5c669523193fcb792c
Michael DeWitt <mdewitt@enova.com>
2013-05-23 13:56:14 -0500

    scaffolding

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Churnhub::Application.config.secret_token = '1baac402db8aaf0ad13811aa6063a7825a2d5074e597b8ed5edfb6690d7c6966d4f46ad21123df738374601e7706479ed321dcc15790499103e797c34b2c74b7'
