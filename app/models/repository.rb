8e73dd01326d038917f54ee48955b72f17d490aa
Michael DeWitt <mdewitt@enova.com>
2013-05-29 01:51:58 -0500

    actually fetch repo stats

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,5 +1,20 @@
 class Repository < ActiveRecord::Base
+  attr_accessor :files
   attr_accessible :url
 
   validates :url, presence: true
+
+  def fetch_from_github!
+    uri = URI(self.url)
+    g = Github.new do |config|
+        if uri.host != "github.com"
+          config.endpoint    = 'https://#{uri.host}/api/v3'
+          config.site        = 'https://#{uri.host}'
+        end
+#       config.oauth_token = ''
+    end
+
+    response = g.repos.commits.compare *uri.path.match(%r{/([^/]+)/([^\./]+)}).captures, "HEAD~1000", "HEAD"
+    self.files = response.files
+  end
 end
