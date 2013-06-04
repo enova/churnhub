5b82f71bcc7e9601d47b282ff912456efb85bde6
jpeterson <jeff@petersonj.com>
2013-06-03 21:07:32 -0500

    render form if invalid

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -6,6 +6,10 @@ class RepositoriesController < ApplicationController
 
   def create
     @repository = Repository.with_url(params[:repository][:url])
-    redirect_to commits_path(@repository.url)
+    if @repository.valid?
+      redirect_to commits_path(@repository.url)
+    else
+      render 'index'
+    end
   end
 end
