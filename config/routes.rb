dbabdf33bf72326eff66f47e5d045e7c34f2eeb3
jpeterson <jeff@petersonj.com>
2013-06-03 20:38:31 -0500

    add commits route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Churnhub::Application.routes.draw do
   resources :repositories, except: [:show]
 
+  get '/*url/commits(/:start(/to/:finish))' => "commits#index", as: :commits
   get "/repo/*url" => "repositories#show", as: :repo
 
   root to: "repositories#index"
