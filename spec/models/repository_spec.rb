a116fd4d9d35df305c45f15e955e56b8a9a2f48e
jpeterson <jeff@petersonj.com>
2013-06-05 14:32:26 -0500

    update specs

--- spec/models/repository_spec.rb
+++ spec/models/repository_spec.rb
@@ -6,21 +6,16 @@ describe Repository do
     repo.url.should == "git.ab.com/u/r"
   end
 
-  describe :with_url do
-    it "creates non-existant repos", :vcr do
+  describe :with_url, :vcr do
+    it "creates non-existant repos" do
       expect {Repository.with_url "github.com/jeffpeterson/irc"}.to change{Repository.count}.by(1)
     end
   end
 
-  it "caches files in db", :vcr do
-    repo = Repository.with_url "github.com/jeffpeterson/irc"
-    repo.files.length.should be > 0
-  end
-
   describe :clean_url do
     it "passes clean urls through" do
-      Repository.clean_url("git.ab.com/u/r").should == "git.ab.com/u/r"
-      Repository.clean_url("github.com/DmitryBaranovskiy/g.raphael").should == "github.com/DmitryBaranovskiy/g.raphael"
+      Repository.clean_url("git.ab.com/u/r").should       == "git.ab.com/u/r"
+      Repository.clean_url("git.com/DB/g.raphael").should == "git.com/DB/g.raphael"
     end
 
     it "removes ssh-style git urls" do
--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -1,12 +1,12 @@
 VCR.configure do |c|
   c.cassette_library_dir = Rails.root.join("spec", "cassettes")
   c.hook_into :webmock
-  # c.filter_sensitive_data('<WSDL>') { "http://www.webservicex.net:80/uszip.asmx?WSDL" }
 end
 
-RSpec.configure do |c|
-  c.treat_symbols_as_metadata_keys_with_true_values = true
-  c.around(:each, :vcr) do |example|
+RSpec.configure do |config|
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+
+  config.around(:each, :vcr) do |example|
     name = example.metadata[:full_description].split(/\s+/, 2).join("/").underscore.gsub(/[^\w\/]+/, "_")
     VCR.use_cassette(name) { example.call }
   end
