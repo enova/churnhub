6587b131464f26369657de3287ee963b68028238
Michael DeWitt <mdewitt@enova.com>
2013-06-12 09:41:30 -0500

    make commit completion conditional
    fetch_files_from_github_if_incomplete! used to make the api call
    every time it was called, regardless of whether it was necessary.

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -6,6 +6,7 @@ class Commit < ActiveRecord::Base
   scope :between, ->(start, finish){ where(timestamp: start..finish)}
 
   def fetch_files_from_github_if_incomplete!
+    return if self.timestamp || self.files
     self.timestamp, self.files = repository.github.commit_by_sha(sha).values_at :timestamp, :files
     save
   end
