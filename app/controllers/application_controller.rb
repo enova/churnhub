46f40067603f525d5819b763d01a6b6a7a23134f
Michael DeWitt <mdewitt@enova.com>
2013-06-14 02:27:38 -0500

    stop slamming github for user data on before_filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :store_target_url
-  before_filter :get_user_data
 
   protected
-
-  def get_user_data
-    @user = Churnhub::Github.user_details session[:access_token] if session[:access_token]
-  end
 
   def store_target_url
     session[:target_url] = request.url.sub /\.json/, ''
